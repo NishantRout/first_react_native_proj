666ccacce832c40fe7ac588eb00a42049a27fd46
NishantRout <48598347+NishantRout@users.noreply.github.com>
2020-09-20 18:40:01 +0530

    Flat List

--- App.js
+++ App.js
@@ -1,19 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  const [people, setPeople] = useState([
+    { name: 'shaun', id: '1' },
+    { name: 'yoshi', id: '2' },
+    { name: 'bruce', id: '3' },
+    { name: 'steve', id: '4' },
+    { name: 'tony', id: '5' },
+    { name: 'harry', id: '6' },
+    { name: 'clint', id: '7' },
+    { name: 'wanda', id: '8' },
+    { name: 'logan', id: '9' },
+  ])
+
+  const pressHandler = (id) => {
+    console.log(id);
+    setPeople((prevPeople) => {
+      return (
+        prevPeople.filter(person => person.id != id)
+      )
+    });
+  }
+
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.boldText}>Hello World !!</Text>
-      </View>
-      <View style={styles.body}>
-        <Text>Lorem Ipsum</Text>
-        <Text>Lorem Ipsum</Text>
-        <Text>Lorem Ipsum</Text>
-      </View>
-      <StatusBar style="auto" />
+      <FlatList
+        numColumns={2}
+        keyExtractor={(item) => item.id}
+        data={people}
+        renderItem={({ item }) => (
+          <TouchableOpacity onPress={() => pressHandler(item.id)}>
+            <Text style={styles.item}> {item.name} </Text>
+          </TouchableOpacity>
+        )}
+      />
+      {/* <ScrollView>
+        {people.map(person => (
+          <View key={person.key}>
+            <Text style={styles.item}> {person.name} </Text>
+          </View>
+        ))}
+      </ScrollView> */}
     </View>
   );
 }
@@ -22,18 +50,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 40,
+    paddingHorizontal: 20,
   },
-  header: {
+  item: {
+    marginTop: 24,
+    padding: 30,
     backgroundColor: '#de5145',
-    padding: 20
-  },
-  boldText: {
-    fontWeight: 'bold',
-  },
-  body: {
-    backgroundColor: 'yellow',
-    padding: 20
+    fontSize: 24,
   }
 });
